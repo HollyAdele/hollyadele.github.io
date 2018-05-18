3819d1cc4c65b1d404d5d18b6576cf15442e69b2
Holly Braun <hbraun25@gmail.com>
2018-05-17 22:26:47 -0400

    uploading the changes Heidi made

--- apps/cakes-by-heidi.js
+++ apps/cakes-by-heidi.js
@@ -1,35 +1,19 @@
-function toStrawberry() {
-  document.getElementById("frosting").src = "https://hollybraun.com/apps/cakes-by-heidi/strawberry-cake.png";
-}
-
- function toBlueberry() {
-  document.getElementById("frosting").src = "https://hollybraun.com/apps/cakes-by-heidi/blueberry-cake.png";
-}
-
 function toMint() {
-  document.getElementById("frosting").src = "https://hollybraun.com/apps/cakes-by-heidi/mint-cake.png";
+  document.getElementById("cake").src="https://www.hollybraun.com/apps/cakes-by-heidi/mint-cake.png"
+}
+
+function toStrawberry() {
+  document.getElementById("mint").src="https://www.hollybraun.com/apps/cakes-by-heidi/strawberry-cake.png"
+}
+
+function toBlueberry() {
+  document.getElementById("mint").src="https://www.hollybraun.com/apps/cakes-by-heidi/Blueberry-cake.png"
 }
 
 function toChocolate() {
-  document.getElementById("frosting").src = "https://hollybraun.com/apps/cakes-by-heidi/chocolate-cake.png";
+  document.getElementById("mint").src="https://www.hollybraun.com/apps/cakes-by-heidi/chocolate-cake.png"
 }
 
 function toVanilla() {
-  document.getElementById("frosting").src = "https://hollybraun.com/apps/cakes-by-heidi/vanilla-cake.png";
-}
-
-function hideCandle() {
-  document.getElementById("candle1").style.visibility= "hidden";
-  document.getElementById("candleOut").style.visibility= "hidden";
-}
-
-hideCandle();
-
-function showCandle() {
-  document.getElementById("candle1").style.visibility = "visible";
-}
-
-function blowOutCandle() {
-  hideCandle();
-  document.getElementById("candleOut").style.visibility = "visible";
+  document.getElementById("mint").src="https://www.hollybraun.com/apps/cakes-by-heidi/mint-cake.png"
 }
